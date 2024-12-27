3eff917992d88a7c3b7c233262b4efb947598a38
Nikolay Kuznetsov <nikolay.kuznetsov.ru@gmail.com>
2024-12-27 19:42:50 +0200

    support sql.Tx in writer interface

---
+++ types/tx.go
@@ -0,0 +1,4 @@
+package types
+
+// Tx is a transaction interface to support both and pgx.Tx and *sql.Tx.
+type Tx interface{}
--- writer.go
+++ writer.go
@@ -2,6 +2,7 @@ package outbox
 
 import (
 	"context"
+	"database/sql"
 	"errors"
 	"fmt"
 
@@ -12,12 +13,13 @@ import (
 
 // Writer writes outbox messages to a single outbox table.
 // To write messages to multiple outbox tables, create multiple Writer instances.
-// An outbox message must be written in the same transaction as the business data, hence the pgx.Tx argument.
+// An outbox message must be written in the same transaction as the business data,
+// hence the tx argument which supports both pgx.Tx and *sql.Tx.
 // Implementations must be safe for concurrent use by multiple goroutines.
 type Writer interface {
 	// Write writes the message to the outbox table.
 	// It returns the ID of the newly inserted message.
-	Write(ctx context.Context, tx pgx.Tx, message types.Message) (int64, error)
+	Write(ctx context.Context, tx types.Tx, message types.Message) (int64, error)
 
 	// TODO: add WriteBatch?
 }
@@ -35,10 +37,10 @@ func NewWriter(table string) (Writer, error) {
 }
 
 // Write returns an error if
-// - tx is nil
+// - tx is nil or unsupported
 // - message is invalid
 // - write operation fails.
-func (w *writer) Write(ctx context.Context, tx pgx.Tx, message types.Message) (int64, error) {
+func (w *writer) Write(ctx context.Context, tx types.Tx, message types.Message) (int64, error) {
 	if tx == nil {
 		return 0, errors.New("tx is nil")
 	}
@@ -53,15 +55,39 @@ func (w *writer) Write(ctx context.Context, tx pgx.Tx, message types.Message) (i
 		Values(message.Broker, message.Topic, message.Metadata, string(message.Payload)).
 		Suffix("RETURNING id")
 
-	q, args, err := ib.ToSql()
+	query, args, err := ib.ToSql()
 	if err != nil {
 		return 0, fmt.Errorf("ib.ToSql: %w", err)
 	}
 
+	row, err := queryRow(ctx, tx, query, args...)
+	if err != nil {
+		return 0, fmt.Errorf("queryRow: %w", err)
+	}
+
 	var id int64
-	if err := tx.QueryRow(ctx, q, args...).Scan(&id); err != nil {
-		return 0, fmt.Errorf("tx.Exec: %w", err)
+	if err := row.Scan(&id); err != nil {
+		return 0, fmt.Errorf("row.Scan: %w", err)
 	}
 
 	return id, nil
+}
+
+func queryRow(ctx context.Context, tx types.Tx, q string, args ...interface{}) (pgx.Row, error) {
+	if tx == nil {
+		return nil, errors.New("tx is nil")
+	}
+
+	var row pgx.Row
+
+	switch t := tx.(type) {
+	case *sql.Tx:
+		row = t.QueryRowContext(ctx, q, args...)
+	case pgx.Tx:
+		row = t.QueryRow(ctx, q, args...)
+	default:
+		return nil, errors.New("unsupported transaction type")
+	}
+
+	return row, nil
 }
